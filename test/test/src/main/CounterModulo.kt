3bdbd981654ec1a50ea5399fea9fec5f829e5824
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2022-05-04 12:01:33 +0100

    Counter refactor

--- test/test/src/main/CounterModulo.kt
+++ test/test/src/main/CounterModulo.kt
@@ -9,24 +9,16 @@ class CounterModulo(private val moduloValue: Int) {
     fun increment(): Int {
         do {
             val valueRead = value.get()
-            if (valueRead != moduloValue - 1) {
-                return value.incrementAndGet()
-            } else {
-                if (value.compareAndSet(valueRead, 0))
-                    return 0
-            }
+            if (valueRead != moduloValue - 1) return value.incrementAndGet()
+            if (value.compareAndSet(valueRead, 0)) return 0
         } while (true)
     }
 
     fun decrement(): Int {
         do {
             val valueRead = value.get()
-            if (value.get() != 1) {
-                return value.decrementAndGet()
-            } else {
-                if (value.compareAndSet(valueRead, moduloValue - 1))
-                    return moduloValue - 1
-            }
+            if (value.get() != 1) return value.decrementAndGet()
+            if (value.compareAndSet(valueRead, moduloValue - 1)) return moduloValue - 1
         } while (true)
     }
 }
